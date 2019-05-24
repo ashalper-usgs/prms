5e430e6dec143f153dfb79f8c6da115a23caaf8f
Andrew Halper <ashalper@usgs.gov>
2019-05-24 14:36:37 -0700

    Cleaned up style and removed empty comments.

--- mmf/build_lists.c
+++ mmf/build_lists.c
@@ -10,85 +10,67 @@
 #include "structs.h"
 #include "defs.h"
 
-#define INPUT  1
-#define OUTPUT  2
+#define INPUT 1
+#define OUTPUT 2
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : ALLOC_list
- | COMMENT      :
- | PARAMETERS   :
- | RETURN VALUE : 
- | RESTRICTIONS :
+  | FUNCTION     : ALLOC_list
 \*--------------------------------------------------------------------*/
 LIST *ALLOC_list (char *name, int type, int size) {
 
-	LIST	*list;
+  LIST	*list;
 
-	list = (LIST *)malloc (sizeof (LIST));
+  list = (LIST *)malloc (sizeof (LIST));
 
-	if (name)
-		list->name = strdup (name);
-	else
-		list->name = NULL;
+  if (name)
+    list->name = strdup (name);
+  else
+    list->name = NULL;
 
-	list->size = size;
-	list->count = 0;
-	list->type = type;
-	list->out = FALSE;
-	list->user_data = NULL;
-	if (size)
-		list->itm = (void **)malloc (size * sizeof (void *));
-	else 
-		list->itm = NULL;
+  list->size = size;
+  list->count = 0;
+  list->type = type;
+  list->out = FALSE;
+  list->user_data = NULL;
+  if (size)
+    list->itm = (void **)malloc (size * sizeof (void *));
+  else 
+    list->itm = NULL;
 
-	return (list);
+  return (list);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : DELETE_list
- | COMMENT      :
- | PARAMETERS   :
- | RETURN VALUE : 
- | RESTRICTIONS :
+  | FUNCTION     : DELETE_list
 \*--------------------------------------------------------------------*/
 void DELETE_list (LIST *list) {
+  int i;
 
-	int		i;
+  if (list->name)
+    free (list->name);
 
-	if (list->name)
-		free (list->name);
+  for (i = 0; i < list->count; i++)
+    if (list->itm[i])
+      free (list->itm[i]);
 
-	for (i = 0; i < list->count; i++)
-		if (list->itm[i])
-			free (list->itm[i]);
-
-	free (list->itm);
-	free (list);
+  free (list->itm);
+  free (list);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : RESIZE_list
- | COMMENT      :
- | PARAMETERS   :
- | RETURN VALUE : 
- | RESTRICTIONS :
+  | FUNCTION     : RESIZE_list
 \*--------------------------------------------------------------------*/
 void RESIZE_list (LIST *list, int new_size) {
-	list->size = new_size;
-	list->itm = (void **)realloc (list->itm, new_size * sizeof (void *));
+  list->size = new_size;
+  list->itm = (void **)realloc (list->itm, new_size * sizeof (void *));
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : ADD_to_list
- | COMMENT      :
- | PARAMETERS   :
- | RETURN VALUE : 
- | RESTRICTIONS :
+  | FUNCTION     : ADD_to_list
 \*--------------------------------------------------------------------*/
 void ADD_to_list (LIST *list, void *itm) {
-	if (list->count >= list->size)
-		RESIZE_list (list, list->size + 100);
+  if (list->count >= list->size)
+    RESIZE_list (list, list->size + 100);
 
-	list->itm[list->count++] = itm;
+  list->itm[list->count++] = itm;
 }
-
