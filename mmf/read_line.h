4e58d9446f9d08c27b4c352bdb7dfc24ad29aa09
Andrew Halper <ashalper@usgs.gov>
2019-05-28 16:06:19 -0700

    Added new prototypes.

--- mmf/read_line.h
+++ mmf/read_line.h
@@ -1,3 +1,11 @@
+long read_line (void);
+
+char *DATA_read_init (void);
+
+char *DATA_check_start (void);
+
+void DATA_close (void);
+
 int control_var_size (char *);
 
 FILE_DATA * FILE_with_next_ts (void);
@@ -5,3 +13,5 @@ FILE_DATA * FILE_with_next_ts (void);
 char * EXTRACT_time (FILE_DATA *);
 
 int CHECK_data (int, FILE_DATA *);
+
+void DATA_find_end (DATETIME *, DATETIME *);
