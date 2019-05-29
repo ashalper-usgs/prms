ec6aa5a94a3e58de8d302dd363489755b728a2c6
Andrew Halper <ashalper@usgs.gov>
2019-05-29 11:33:57 -0700

    White space changes only. No functional changes.

--- mmf/graph_single_run.c
+++ mmf/graph_single_run.c
@@ -29,7 +29,7 @@ int *disp_ele;
 int numDispVars;
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : initializeRuntimeGraphs
+  | FUNCTION     : initializeRuntimeGraphs
 \*--------------------------------------------------------------------*/
 int initializeRuntimeGraphs (void) {
   CONTROL *control;
@@ -50,94 +50,94 @@ int initializeRuntimeGraphs (void) {
 
   zero_time = starttime_copy.jt;
 
-/*
-** Get the number of display vars
-*/
-   cptr = strdup ("dispVar_names");
-   control = control_addr(cptr);
-   if (control) {
-      numDispVars = control->size;
+  /*
+  ** Get the number of display vars
+  */
+  cptr = strdup ("dispVar_names");
+  control = control_addr(cptr);
+  if (control) {
+    numDispVars = control->size;
 
-      disp_var = (PUBVAR **)malloc (sizeof(PUBVAR *) * numDispVars);
-      memset (disp_var, 0, sizeof (PUBVAR *) * numDispVars);
+    disp_var = (PUBVAR **)malloc (sizeof(PUBVAR *) * numDispVars);
+    memset (disp_var, 0, sizeof (PUBVAR *) * numDispVars);
 
-      disp_ele = (int *)malloc (sizeof (int) * numDispVars);
-      memset (disp_ele, 0, sizeof (int) * numDispVars);
+    disp_ele = (int *)malloc (sizeof (int) * numDispVars);
+    memset (disp_ele, 0, sizeof (int) * numDispVars);
 
-      for (i = 0; i < numDispVars; i++) {
-/**
-** Get address of each display variable for each graph
-**/
-         cptr = strdup ("dispVar_names");
-		 cptr2 = (char *)control_sarray(cptr, i);
+    for (i = 0; i < numDispVars; i++) {
+      /**
+       ** Get address of each display variable for each graph
+       **/
+      cptr = strdup ("dispVar_names");
+      cptr2 = (char *)control_sarray(cptr, i);
 
-         disp_var[i] = var_addr (cptr2);
+      disp_var[i] = var_addr (cptr2);
 
-         cptr = strdup ("dispVar_element");
-         disp_ele[i] =  atoi (control_sarray(cptr,i)) - 1;
-      }
-   } else {
-	   numDispVars = 0;
-	   disp_var = NULL;
-	   disp_ele = NULL;
-	   runtime_graph_on = 0;
-   }
-   return (FALSE);
+      cptr = strdup ("dispVar_element");
+      disp_ele[i] =  atoi (control_sarray(cptr,i)) - 1;
+    }
+  } else {
+    numDispVars = 0;
+    disp_var = NULL;
+    disp_ele = NULL;
+    runtime_graph_on = 0;
+  }
+  return (FALSE);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : plotRuntimeGraphValue
+  | FUNCTION     : plotRuntimeGraphValue
 \*--------------------------------------------------------------------*/
 int plotRuntimeGraphValue (void) {
-   double xval;
-   float yval;
-   int i;
-   DATETIME nowtime_copy;
+  double xval;
+  float yval;
+  int i;
+  DATETIME nowtime_copy;
 
-   if (!runtime_graph_on) return (FALSE);
+  if (!runtime_graph_on) return (FALSE);
 
-   nowtime_copy.year =Mnowtime->year;
-   nowtime_copy.month =Mnowtime->month;
-   nowtime_copy.day = Mnowtime->day;
-   nowtime_copy.hour =Mnowtime->hour;
-   nowtime_copy.min =Mnowtime->min;
-   nowtime_copy.sec =Mnowtime->sec;
+  nowtime_copy.year =Mnowtime->year;
+  nowtime_copy.month =Mnowtime->month;
+  nowtime_copy.day = Mnowtime->day;
+  nowtime_copy.hour =Mnowtime->hour;
+  nowtime_copy.min =Mnowtime->min;
+  nowtime_copy.sec =Mnowtime->sec;
 
-   julday(&nowtime_copy);
+  julday(&nowtime_copy);
 
-   xval = nowtime_copy.jt - zero_time;
+  xval = nowtime_copy.jt - zero_time;
 
-   printf ("plotRuntimeGraphValue: xval = %f", xval);
+  printf ("plotRuntimeGraphValue: xval = %f", xval);
 
-   for (i = 0; i < numDispVars; i++) {
-      yval = 0.0;
-      switch ((disp_var[i])->type) {
-         case M_LONG :
-            yval = (float)(*(((long *)((disp_var[i])->value)) + disp_ele[i]));
-            break;
+  for (i = 0; i < numDispVars; i++) {
+    yval = 0.0;
+    switch ((disp_var[i])->type) {
+    case M_LONG :
+      yval = (float)(*(((long *)((disp_var[i])->value)) + disp_ele[i]));
+      break;
 
-         case M_DOUBLE :
-            yval = (float)(*(((double *)((disp_var[i])->value)) + disp_ele[i]));
-            break;
+    case M_DOUBLE :
+      yval = (float)(*(((double *)((disp_var[i])->value)) + disp_ele[i]));
+      break;
 
-         case M_FLOAT :
-            yval = *(((float *)((disp_var[i])->value)) + disp_ele[i]);
-            break;
-      }
-      printf (" %f", yval);
-   }
-   printf ("\n");
-   fflush (stdout);
+    case M_FLOAT :
+      yval = *(((float *)((disp_var[i])->value)) + disp_ele[i]);
+      break;
+    }
+    printf (" %f", yval);
+  }
+  printf ("\n");
+  fflush (stdout);
 
-   return (FALSE);
+  return (FALSE);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : closeRuntimeGraphs
+  | FUNCTION     : closeRuntimeGraphs
 \*--------------------------------------------------------------------*/
 int closeRuntimeGraphs (void) {
-   if (!runtime_graph_on) return (FALSE);
+  if (!runtime_graph_on) return (FALSE);
 
-   printf ("closeRuntimeGraph\n");
-   return (FALSE);
+  printf ("closeRuntimeGraph\n");
+  return (FALSE);
 }
