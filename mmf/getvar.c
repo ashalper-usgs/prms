e8840e4e305b43991a1242f9543d842625ac792c
Andrew Halper <ashalper@usgs.gov>
2019-05-29 11:33:37 -0700

    Style clean-up.

--- mmf/getvar.c
+++ mmf/getvar.c
@@ -16,201 +16,207 @@
 long getvar (char *, char *, long, char *, double *);
 
 /*--------------------------------------------------------------------*\
- | FUNCTION		: getvar_
- | COMMENT		: called from Fortran, sorts out args and calls getvar()
+  | FUNCTION		: getvar_
+  | COMMENT		: called from Fortran, sorts out args and
+  |                       calls getvar()
 \*--------------------------------------------------------------------*/
-long getvar_ (char *mname, char *vname, ftnint *vmaxsize, char *vtype, double *value, ftnlen mnamelen, ftnlen vnamelen, ftnlen vtypelen) {
-	char module[80], name[80], type[80];
-	long maxsize, retval;
+long getvar_ (char *mname, char *vname, ftnint *vmaxsize, char *vtype,
+	      double *value, ftnlen mnamelen, ftnlen vnamelen,
+	      ftnlen vtypelen) {
+  char module[80], name[80], type[80];
+  long maxsize, retval;
   
-/*
-* copy size to local long int
-* copy args to new strings, and terminate
-*/
-	maxsize = *vmaxsize;
+  /*
+   * copy size to local long int
+   * copy args to new strings, and terminate
+   */
+  maxsize = *vmaxsize;
 
-	strncpy (module, mname, mnamelen);
-	*(module + mnamelen) = '\0';
+  strncpy (module, mname, mnamelen);
+  *(module + mnamelen) = '\0';
 
-	strncpy (name, vname, vnamelen);
-	*(name + vnamelen) = '\0';
+  strncpy (name, vname, vnamelen);
+  *(name + vnamelen) = '\0';
 
-	strncpy (type, vtype, vtypelen);
-	*(type + vtypelen) = '\0';
+  strncpy (type, vtype, vtypelen);
+  *(type + vtypelen) = '\0';
   
-/*
-* call C version of getvar()
-*/
-	retval =  getvar (module, name, maxsize, type, value);
+  /*
+   * call C version of getvar()
+   */
+  retval =  getvar (module, name, maxsize, type, value);
   
-	return (retval);
+  return (retval);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : getvar
- | COMMENT	: called from C
-\*--------------------------------------------------------------------*/
-long getvar (char *module, char *name, long maxsize, char *type, double *value) {
+  | FUNCTION     : getvar
+  | COMMENT	: called from C
+  \*--------------------------------------------------------------------*/
+long getvar (char *module, char *name, long maxsize, char *type,
+	     double *value) {
 
-	int var_type;
-	PUBVAR *var;
-	char vkey[128];
-	long i;
-	long n1, n2;
-	char *ptr1;
-	char *ptr2;
+  int var_type;
+  PUBVAR *var;
+  char vkey[128];
+  long i;
+  long n1, n2;
+  char *ptr1;
+  char *ptr2;
 
-/*
-* compute the key
-*/
+  /*
+   * compute the key
+   */
   
-   strncpy (vkey, name, 128);
+  strncpy (vkey, name, 128);
   
-/*
-* convert Fortran types to C types
-*/
+  /*
+   * convert Fortran types to C types
+   */
 
-	var_type = M_LONG;
-	if (!strcmp(type, "real") || !strcmp(type, "float"))
-	  var_type = M_FLOAT;
-	else if (!strcmp(type, "double precision") || !strcmp(type, "double"))
-	  var_type = M_DOUBLE;
+  var_type = M_LONG;
+  if (!strcmp(type, "real") || !strcmp(type, "float"))
+    var_type = M_FLOAT;
+  else if (!strcmp(type, "double precision") || !strcmp(type, "double"))
+    var_type = M_DOUBLE;
 
-/*
-* check that type is possible
-*/
-	if((var_type != M_LONG) && (var_type != M_FLOAT) && (var_type != M_DOUBLE)){
-		(void)fprintf(stderr,
-				"ERROR - getvar - type %s is illegal.\n", type);
-		(void)fprintf(stderr, "Key is '%s'.\n", vkey);
-		(void)fprintf(stderr, "Type is '%s'.\n", type);
-		return(1);
-	}
+  /*
+   * check that type is possible
+   */
+  if((var_type != M_LONG) && (var_type != M_FLOAT) && (var_type != M_DOUBLE)){
+    (void)fprintf(stderr,
+		  "ERROR - getvar - type %s is illegal.\n", type);
+    (void)fprintf(stderr, "Key is '%s'.\n", vkey);
+    (void)fprintf(stderr, "Type is '%s'.\n", type);
+    return(1);
+  }
   
-/*
-* get pointer to variable with key
-*/
-	if (!(var = var_addr (vkey))) {
-		(void)fprintf(stderr, "ERROR - getvar - variable not found.\n");
-		(void)fprintf(stderr, "Key:   '%s'\n", vkey);
-		return(1);
-	}
+  /*
+   * get pointer to variable with key
+   */
+  if (!(var = var_addr (vkey))) {
+    (void)fprintf(stderr, "ERROR - getvar - variable not found.\n");
+    (void)fprintf(stderr, "Key:   '%s'\n", vkey);
+    return(1);
+  }
   
-/*
-* check that there is enough space allocated in the calling routine
-* to accommodate the data
-*/
+  /*
+   * check that there is enough space allocated in the calling routine
+   * to accommodate the data
+   */
   
-	if (var->size > maxsize) {
-		(void)fprintf (stderr, 
-	    			"ERROR - getvar - insufficient space for data transfer.\n");
-		(void)fprintf(stderr, "Key:   '%s'\n", vkey);
-		(void)fprintf(stderr, "Actual size in data base: %ld\n", var->size);
-		(void)fprintf(stderr, "Available space in calling routine: %ld\n",
-						maxsize);
-		return(1);
-	}
+  if (var->size > maxsize) {
+    (void)fprintf (stderr, 
+		   "ERROR - getvar - insufficient space for data transfer.\n");
+    (void)fprintf(stderr, "Key:   '%s'\n", vkey);
+    (void)fprintf(stderr, "Actual size in data base: %ld\n", var->size);
+    (void)fprintf(stderr, "Available space in calling routine: %ld\n",
+		  maxsize);
+    return(1);
+  }
 
-/*
-* copy the variable across
-*/
+  /*
+   * copy the variable across
+   */
   
-	if (var->ndimen == 1) {
-		switch (var->type) {
-			case M_LONG:
-				memcpy ((char *)value, (char *)var->value,
-							var->size * sizeof(int));
-				break;
+  if (var->ndimen == 1) {
+    switch (var->type) {
+    case M_LONG:
+      memcpy ((char *)value, (char *)var->value,
+	      var->size * sizeof(int));
+      break;
       
-			case M_FLOAT:
-				memcpy ((char *)value, (char *)var->value,
-							var->size * sizeof(float));
-				break;
+    case M_FLOAT:
+      memcpy ((char *)value, (char *)var->value,
+	      var->size * sizeof(float));
+      break;
       
-			case M_DOUBLE:
-				memcpy ((char *)value, (char *)var->value,
-							var->size * sizeof(double));
-				break;
-		}
-	} else if (var->ndimen ==2) {
-		n1 = var->dimen[0]->value;
-		n2 = var->dimen[1]->value;
-		if (n1*n2!=maxsize) n1 = var->dimen[0]->max;
-/*rsr added next block*/
-		if (n1*n2 > maxsize ) {
-			n1 = var->dimen[0]->value;
-		}
-/*rsr end block*/
-		ptr1 = var->value;
-		ptr2 = (char *)value;
+    case M_DOUBLE:
+      memcpy ((char *)value, (char *)var->value,
+	      var->size * sizeof(double));
+      break;
+    }
+  } else if (var->ndimen ==2) {
+    n1 = var->dimen[0]->value;
+    n2 = var->dimen[1]->value;
+    if (n1*n2!=maxsize) n1 = var->dimen[0]->max;
+    /*rsr added next block*/
+    if (n1*n2 > maxsize ) {
+      n1 = var->dimen[0]->value;
+    }
+    /*rsr end block*/
+    ptr1 = var->value;
+    ptr2 = (char *)value;
 
-		for (i = 0; i < n2; i++) {
+    for (i = 0; i < n2; i++) {
 
-			switch (var->type) {
-				case M_LONG:
-					memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(int));
-					ptr1 += n1 * sizeof(int);
-					ptr2 += n1 * sizeof(int);
-					break;
+      switch (var->type) {
+      case M_LONG:
+	memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(int));
+	ptr1 += n1 * sizeof(int);
+	ptr2 += n1 * sizeof(int);
+	break;
 
-				case M_FLOAT:
-					memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(float));
-					ptr1 += n1 * sizeof(float);
-					ptr2 += n1 * sizeof(float);
-					break;
+      case M_FLOAT:
+	memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(float));
+	ptr1 += n1 * sizeof(float);
+	ptr2 += n1 * sizeof(float);
+	break;
 
-				case M_DOUBLE:
-					memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(double));
-					ptr1 += n1 * sizeof(double);
-					ptr2 += n1 * sizeof(double);
-					break;
-			}
-		}
-	}
+      case M_DOUBLE:
+	memcpy ((char *)ptr2, (char *)ptr1, n1 * sizeof(double));
+	ptr1 += n1 * sizeof(double);
+	ptr2 += n1 * sizeof(double);
+	break;
+      }
+    }
+  }
 
-	return (0);
+  return (0);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION		: getvartype_
- | COMMENT		: called from Fortran, sorts out args and returns the type()
+  | FUNCTION		: getvartype_
+  | COMMENT		: called from Fortran, sorts out args and
+  |                       returns the type()
 \*--------------------------------------------------------------------*/
 long getvartype_ (char *vname, ftnlen vnamelen) {
-	char vkey[128];
-	PUBVAR *var;
+  char vkey[128];
+  PUBVAR *var;
   
-    strncpy (vkey, vname, 128);
-/*
-* get pointer to variable with key
-*/
-	if (!(var = var_addr (vkey))) {
-		(void)fprintf(stderr, "ERROR - getvartype - variable not found.\n");
-		(void)fprintf(stderr, "Key:   '%s'\n", vkey);
-		return(-1);
-	}
+  strncpy (vkey, vname, 128);
+  /*
+   * get pointer to variable with key
+   */
+  if (!(var = var_addr (vkey))) {
+    (void)fprintf(stderr, "ERROR - getvartype - variable not found.\n");
+    (void)fprintf(stderr, "Key:   '%s'\n", vkey);
+    return(-1);
+  }
 
-	return (var->type);
+  return (var->type);
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION		: getvarsize_
- | COMMENT		: called from Fortran, sorts out args and returns the variable size()
- | RETURN VALUE : size of the array for input variable
- | RESTRICTIONS : variable must be declared
+  | FUNCTION		: getvarsize_
+  | COMMENT		: called from Fortran, sorts out args and
+  |                       returns the variable size()
+  | RETURN VALUE : size of the array for input variable
+  | RESTRICTIONS : variable must be declared
 \*--------------------------------------------------------------------*/
 long getvarsize_ (char *vname, ftnlen vnamelen) {
-	char vkey[128];
-	PUBVAR *var;
+  char vkey[128];
+  PUBVAR *var;
   
-    strncpy (vkey, vname, 128);
-/*
-* get pointer to variable with key
-*/
-	if (!(var = var_addr (vkey))) {
-		(void)fprintf(stderr, "ERROR - getvartype - variable not found.\n");
-		(void)fprintf(stderr, "Key:   '%s'\n", vkey);
-		return(-1);
-	}
+  strncpy (vkey, vname, 128);
+  /*
+   * get pointer to variable with key
+   */
+  if (!(var = var_addr (vkey))) {
+    (void)fprintf(stderr, "ERROR - getvartype - variable not found.\n");
+    (void)fprintf(stderr, "Key:   '%s'\n", vkey);
+    return(-1);
+  }
 
-	return (var->size);
+  return (var->size);
 }
--- mmf/julconvert.c
+++ mmf/julconvert.c
@@ -17,84 +17,87 @@ int isleap (int);
 
 /*
 ** Takes a date, and returns a Julian day. A Julian day is the number of
-** days since some base date  (in the very distant past).
+** days since some base date (in the very distant past).
+**
 ** Handy for getting date of x number of days after a given Julian date
 ** (use jdate to get that from the Gregorian date).
+**
 ** Author: Robert G. Tantzen, translator: Nat Howard
-** Translated from the algol original in Collected Algorithms of CACM
+**
+** Translated from the ALGOL original in *Collected Algorithms of CACM*.
 ** (This and jdate are algorithm 199).
 */
 double getjulday(int mon, int day, int year, int h, int mi, double se) {
-	DATETIME datetime;
+  DATETIME datetime;
 
-	datetime.year = year;
-	datetime.month = mon;
-	datetime.day = day;
-	datetime.hour = h;
-	datetime.min = mi;
-	datetime.sec = se;
-	julday(&datetime);
+  datetime.year = year;
+  datetime.month = mon;
+  datetime.day = day;
+  datetime.hour = h;
+  datetime.min = mi;
+  datetime.sec = se;
+  julday(&datetime);
 
-	return datetime.jt;
+  return datetime.jt;
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : getjulday_
- | COMMENT      : getjulday binding for Fortran
+  | FUNCTION     : getjulday_
+  | COMMENT      : getjulday binding for Fortran
 \*--------------------------------------------------------------------*/
 double getjulday_(int *mon, int *day, int *year, int *h, int *mi, double *se) {
-   return getjulday (*mon, *day, *year, *h, *mi, *se);
+  return getjulday (*mon, *day, *year, *h, *mi, *se);
 }
 
 int dayofweek(double j) {
-    j += 0.5;
-    return (int) (j + 1) % 7;
+  j += 0.5;
+  return (int) (j + 1) % 7;
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : isleap_
- | COMMENT      : isleap binding for Fortran
- | PARAMETERS   : see below
- | RETURN VALUE : see below
+  | FUNCTION     : isleap_
+  | COMMENT      : isleap binding for Fortran
+  | PARAMETERS   : see below
+  | RETURN VALUE : see below
 \*--------------------------------------------------------------------*/
 long isleap_ (ftnint *year) {
-   return ((long)isleap((int)(*year)));
+  return ((long)isleap((int)(*year)));
 }
 
 /*--------------------------------------------------------------------*\
- | FUNCTION     : isleap
- | COMMENT      : Thanks to Rick Webb
- | PARAMETERS   : int year - the year to test
- | RETURN VALUE : Returns 1 if year is a leap year, 0 if not.
- | RESTRICTIONS : Called from C.
+  | FUNCTION     : isleap
+  | COMMENT      : Thanks to Rick Webb
+  | PARAMETERS   : int year - the year to test
+  | RETURN VALUE : Returns 1 if year is a leap year, 0 if not.
+  | RESTRICTIONS : Called from C.
 \*--------------------------------------------------------------------*/
 int isleap (int year) {
 
-   double nptr;
+  double nptr;
 
-/*
-**  Check if leapyear - Start by identifying all years not
-**       divisible by 4
-*/
-   if (modf ((double)year/4.0, &nptr)!=0) {
-      return(0);
-/*
-**  Identify leap years that are not century years
- */
-   } else if (modf((double)year/4.0, &nptr)==0 && modf(year/100.0, &nptr)!=0 ) {
-      return(1);
+  /*
+  **  Check if leapyear - Start by identifying all years not
+  **       divisible by 4
+  */
+  if (modf ((double)year/4.0, &nptr)!=0) {
+    return(0);
+    /*
+    **  Identify leap years that are not century years
+    */
+  } else if (modf((double)year/4.0, &nptr)==0 && modf(year/100.0, &nptr)!=0 ) {
+    return(1);
 
-/*
-**  century years are not leap years unless divisible by 400
-*/
-   } else if (modf((double)year/400.0, &nptr)!=0 ) {
-      return(0);
+    /*
+    **  century years are not leap years unless divisible by 400
+    */
+  } else if (modf((double)year/400.0, &nptr)!=0 ) {
+    return(0);
 
-/*
-**  all that's left are century years divisible by 400 which
-**         are also leap years
-*/
-   } else {
-      return(1);
-   }
+    /*
+    **  all that's left are century years divisible by 400 which
+    **         are also leap years
+    */
+  } else {
+    return(1);
+  }
 }
