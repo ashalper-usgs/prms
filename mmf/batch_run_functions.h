6e1f4c4fa9b1d6472e9872f5dbf837fbea5c7a03
Andrew Halper <ashalper@usgs.gov>
2019-05-29 15:11:46 -0700

    Initial version.

---
+++ mmf/batch_run_functions.h
@@ -0,0 +1,11 @@
+char *single_run_pre_init (void);
+
+char *single_run_post_init (void);
+
+char *single_run_pre_run (void);
+
+char *single_run_post_run (void);
+
+char *single_run_pre_cleanup (void);
+
+char *single_run_post_cleanup (void);
