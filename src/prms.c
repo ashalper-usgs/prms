d02b085a140a7a12beaff0d4c7241ba6dcf0ea17
Andrew Halper <ashalper@usgs.gov>
2019-05-29 15:11:31 -0700

    Assimilated two modules that were not called from anywhere else.

--- src/prms.c
+++ src/prms.c
@@ -6,18 +6,23 @@
 #define MAIN
 #define MMF_C
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+
 #include "defs.h"
 #include "structs.h"
 #include "globals.h"
-#include "alloc_space.h"
+
 #include "batch_run.h"
+#include "batch_run_functions.h"
 #include "build_lists.h"
+#include "call_modules.h"
 #include "control_addr.h"
 #include "control_var.h"
+#include "decldim.h"
 #include "get_times.h"
 #include "parse_args.h"
 #include "print_model_info.h"
@@ -28,6 +33,7 @@
 #include "read_params.h"
 #include "save_params.h"
 #include "setup_cont.h"
+#include "umalloc_etc.h"
 
 /* in globals.c */
 extern PARAM **unsort_params;
@@ -50,11 +56,13 @@ extern int preprocess_on;
 extern int call_modules(char *);
 extern int call_setdims(void);
 
+void alloc_space (void);
+
 LIST *module_db;
 MODULE_DATA *current_module;
 FILE_DATA   **fd = NULL;
 double Mprevjt = -1.0; /* the latest previous Julian time  */
-char *Minpptr = NULL;  /* pointer to current posn in data input line*/
+char *Minpptr = NULL;  /* pointer to current position in data input line */
 int  M_stop_run = 0;	      /* Run switch 0 -or 1 */
 STAT_LIST_TYPE *Mfirst_stat_list = NULL; /* pointer to first entry
 					    in stats link list */
@@ -113,7 +121,11 @@ int main (int argc, char *argv[]) {
    parse_args (argc, argv, &set_count, set_name, set_value, set_size);
 
    if (MAltContFile == NULL) {
-     (void)fprintf (stderr,"Usage: Set the full path to the control file using the '-C' option.\n\n");
+     (void)fprintf (
+	     stderr,
+	     "Usage: Set the full path to the control file using "
+	     "the '-C' option.\n\n"
+	   );
      exit(0);
    }
 
@@ -194,7 +206,10 @@ int main (int argc, char *argv[]) {
    num_param_files = control_var_size ("param_file");
 
    if (call_modules("declare")) {
-     (void)fprintf(stderr, "\nERROR: in declare procedure, in function 'call_modules'\n");
+     (void)fprintf(
+	     stderr,
+	     "\nERROR: in declare procedure, in function 'call_modules'\n"
+	   );
      exit (1);
    }
     
@@ -205,13 +220,14 @@ int main (int argc, char *argv[]) {
    num_param_files = control_var_size ("param_file");
 
    /*
-   **  Look for, declare and read in mapping parameters before any of the "module" parameters
+   **  Look for, declare and read in mapping parameters before any of
+   **  the "module" parameters.
    */
    for (i = 0; i < num_param_files; i++) {
      if (stat (fname[i], &stbuf) != -1) {
        if (stbuf.st_size) {
        } else {
-	 (void)fprintf (stderr,buf, "ERROR: Parameter file: %s is empty.",
+	 (void)fprintf (stderr, buf, "ERROR: Parameter file: %s is empty.",
 			fname[i]);
 	 exit (1);
        }
@@ -266,4 +282,145 @@ int main (int argc, char *argv[]) {
    }
 
    exit (0);
+}
+
+/* Global variables, referenced in MMS, C modules for legacy
+   reasons. */
+
+LIST *cont_db;
+LIST *dim_db;
+int max_vars;
+PUBVAR **Mvarbase = NULL;   /* pointer to public variables database */
+long Mnvars = 0;	    /* no of public variables in database */
+int max_params;
+PARAM **Mparambase = NULL;  /* pointer to parameter database */
+long Mnparams = 0;	    /* no of params in database */
+int max_read_vars;
+READCHECK **Mcheckbase = NULL;	/* pointer to read check database */
+long Mnreads = 0;	  /* max no. of calls to be made by readvar */
+DATETIME *Mstrttime = NULL;	/* pointer to start time structure */
+DATETIME *Mendtime = NULL;	/* pointer to end time structure */
+DATETIME *Mnowtime = NULL; /* pointer to current data time structure */
+DATETIME *Mnexttime = NULL;  /* pointer to next data time structure */
+char *Mparaminfo = NULL;     /* pointer to param information string */
+char *Mdatainfo = NULL;	     /* pointer to data information string */
+
+void alloc_space (void) {
+  static DATETIME start, end, now, next;
+
+  cont_db = ALLOC_list ("Control Data Base", 0, 100);
+
+  /*
+   * space for the dimension pointer array
+   */
+
+  dim_db = ALLOC_list ("Dimension Data Base", 0, 50);
+
+  /*
+   * default dimension "one"
+   */
+
+  decldim ("one", 1, 1, "Dimension of scalar parameters and variables");
+
+  /*
+   * space for the public variable pointer array
+   */
+
+  max_vars = 500;
+  Mvarbase = (PUBVAR **) umalloc (max_vars * sizeof(PUBVAR *));
+  Mnvars = 0;
+
+  /*
+   * space for the parameter pointer array
+   */
+
+  max_params = 500;
+  Mparambase = (PARAM **) umalloc (max_params * sizeof(PARAM *));
+  Mnparams = 0;
+
+  /*
+   * space for the read check data base
+   */
+
+  max_read_vars = 50;
+  Mcheckbase = (READCHECK **) umalloc (max_read_vars * sizeof(READCHECK *));
+  Mnreads = 0;
+
+  /*
+   * space for time structures
+   */
+  Mstrttime = &start;
+  Mendtime = &end;
+  Mnowtime = &now;
+  Mnexttime = &next;
+
+  /*
+   * space for run info. string
+   */
+  Mparaminfo = strdup ("Default case");
+  Mdatainfo = strdup ("Default case");
+}
+
+/**
+ * FUNCTION     : BATCH_run
+ * PARAMETERS   : None
+ * RETURN VALUE : int - error code if there is one.
+ * RESTRICTIONS : None
+ * COMMENT  : Runs the MMS time loop.
+ */
+int BATCH_run (void) {
+  char *ret;
+  long endofdata = 0;
+
+  ret = single_run_pre_init ();
+  if (ret) {
+    fprintf (stderr,"%s\n", ret);
+    return(1);
+  }
+
+  if (call_modules("initialize")) {
+    fprintf (stderr, "single_run:  Problem with initializing modules.");
+    return(1);
+  }
+
+  ret = single_run_post_init ();
+  if (ret) return(1);
+
+  /*
+   * perform the main loop
+   */
+
+  M_stop_run = 0;
+  Mprevjt = -1.0;
+
+  while(!endofdata) {
+    if(!(endofdata = read_line ())) {
+      ret = single_run_pre_run ();
+      if (ret) return(1);
+
+      errno = 0;
+
+      if(call_modules("run")) {
+	fprintf (stderr, "Problem while running modules.");
+	return(1);
+      }
+
+      ret = single_run_post_run ();
+      if (ret) return(1);
+    }
+  }
+
+  ret = single_run_post_cleanup ();
+  if (ret) return(1);
+
+  /*
+   * cleanup modules
+   */
+
+  if (call_modules("cleanup")) {
+    fprintf (stderr, "Problem with module cleanup.");
+    return(1);
+  }
+
+  return(0);
 }
